64a4c67965a5a79fcbbc65b531cddeb6d004c6e5
jesuslayton@gmail.com <jesuslayton>
2025-10-04 17:23:18 -0500

    cmabios version anterior

--- vulnerabilities/view_help.php
+++ vulnerabilities/view_help.php
@@ -1,4 +1,5 @@
 <?php
+
 define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
@@ -10,47 +11,18 @@ $page[ 'title' ] = 'Help' . $page[ 'title_separator' ].$page[ 'title' ];
 if (array_key_exists ("id", $_GET) &&
 	array_key_exists ("security", $_GET) &&
 	array_key_exists ("locale", $_GET)) {
-	
 	$id       = $_GET[ 'id' ];
 	$security = $_GET[ 'security' ];
-	$locale   = $_GET[ 'locale' ];
-	
-	// Sanitizar entradas para prevenir Path Traversal
-	$id = basename($id);
-	$id = preg_replace('/[^a-zA-Z0-9_-]/', '', $id);
-	
-	$locale = basename($locale);
-	$locale = preg_replace('/[^a-zA-Z_]/', '', $locale);
-	
-	// Lista blanca de IDs permitidos
-	$allowed_ids = array('brute', 'exec', 'csrf', 'fi', 'sqli', 'sqli_blind', 'upload', 'xss_d', 'xss_r', 'xss_s');
-	$allowed_locales = array('en', 'es', 'fr', 'de', 'zh');
-	
-	if (!in_array($id, $allowed_ids)) {
-		$help = "<p>Invalid help page requested</p>";
-	} elseif (!in_array($locale, $allowed_locales)) {
-		$locale = 'en'; // Default a inglés
+	$locale = $_GET[ 'locale' ];
+
+	ob_start();
+	if ($locale == 'en') {
+		eval( '?>' . file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.php" ) . '<?php ' );
+	} else {
+		eval( '?>' . file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.{$locale}.php" ) . '<?php ' );
 	}
-	
-	if (in_array($id, $allowed_ids)) {
-		ob_start();
-		
-		if ($locale == 'en') {
-			$help_file = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.php";
-		} else {
-			$help_file = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.{$locale}.php";
-		}
-		
-		// Verificar que el archivo existe antes de incluirlo
-		if (file_exists($help_file) && is_file($help_file)) {
-			include($help_file);
-		} else {
-			echo "<p>Help file not found</p>";
-		}
-		
-		$help = ob_get_contents();
-		ob_end_clean();
-	}
+	$help = ob_get_contents();
+	ob_end_clean();
 } else {
 	$help = "<p>Not Found</p>";
 }
@@ -58,9 +30,11 @@ if (array_key_exists ("id", $_GET) &&
 $page[ 'body' ] .= "
 <script src='/vulnerabilities/help.js'></script>
 <link rel='stylesheet' type='text/css' href='/vulnerabilities/help.css' />
+
 <div class=\"body_padded\">
 	{$help}
 </div>\n";
 
 dvwaHelpHtmlEcho( $page );
+
 ?>
