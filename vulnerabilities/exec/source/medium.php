3a3150c64dc0e69b6f5327e67f65fe8a888db2bc
jesusLayton <jesuslayton@gmail.com>
2025-10-07 23:01:35 -0500

    correcion command injection 808

--- vulnerabilities/exec/source/medium.php
+++ vulnerabilities/exec/source/medium.php
@@ -1,30 +1,40 @@
 <?php
-
 if( isset( $_POST[ 'Submit' ]  ) ) {
 	// Get input
-	$target = $_REQUEST[ 'ip' ];
-
-	// Set blacklist
-	$substitutions = array(
-		'&&' => '',
-		';'  => '',
-	);
-
-	// Remove any of the characters in the array (blacklist).
-	$target = str_replace( array_keys( $substitutions ), $substitutions, $target );
-
+	$target = trim($_REQUEST[ 'ip' ]);
+	
+	// Validar que sea una IP o hostname válido
+	$target = filter_var($target, FILTER_VALIDATE_IP);
+	
+	if ($target === false) {
+		// Si no es una IP válida, intentar validar como hostname
+		$target = trim($_REQUEST[ 'ip' ]);
+		// Permitir solo caracteres válidos para hostname (letras, números, guiones, puntos)
+		if (!preg_match('/^[a-zA-Z0-9.-]+$/', $target)) {
+			$html .= "<pre>Error: Invalid IP address or hostname format</pre>";
+			return;
+		}
+		// Validar longitud máxima
+		if (strlen($target) > 253) {
+			$html .= "<pre>Error: Hostname too long</pre>";
+			return;
+		}
+	}
+	
+	// Escapar el argumento para shell
+	$target = escapeshellarg($target);
+	
 	// Determine OS and execute the ping command.
 	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
 		// Windows
-		$cmd = shell_exec( 'ping  ' . $target );
+		$cmd = shell_exec( 'ping ' . $target );
 	}
 	else {
 		// *nix
-		$cmd = shell_exec( 'ping  -c 4 ' . $target );
+		$cmd = shell_exec( 'ping -c 4 ' . $target );
 	}
-
-	// Feedback for the end user
-	$html .= "<pre>{$cmd}</pre>";
+	
+	// Escapar la salida para prevenir XSS
+	$html .= "<pre>" . htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') . "</pre>";
 }
-
 ?>
